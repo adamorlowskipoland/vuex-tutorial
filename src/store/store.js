09ca1784f73f7c200689b7a5ac0da1a1a7a9d4ff
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-10-22 14:31:23 +0200

    MUTATIONS - create mutation, call it on component

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
+  strict: true, /* strict mode doesn't allow to change store directly (without using mutations) */
   state: {
     products: [
       {name: 'Banana Skin', price: 20},
@@ -21,6 +22,13 @@ export const store = new Vuex.Store({
         }
       })
       return saleProducts
+    }
+  },
+  mutations: {
+    reducePrice (state) {
+      state.products.forEach( product => {
+        product.price -= 1
+      })
     }
   }
 })
